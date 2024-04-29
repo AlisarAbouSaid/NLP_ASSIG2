acda6cc73b6182698251c1473b0130a4b049e7cc
Sjors3 <100273270+Sjors3@users.noreply.github.com>
2024-04-29 22:49:26 +0200

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -43,7 +43,7 @@ print("p(jury|the):\t\t", bigram[word_to_index["jury"], word_to_index["the"]])
 print("p(campaign|the):\t", bigram[word_to_index["campaign"], word_to_index["the"]])
 print("p(calls|anonymous):\t", bigram[word_to_index["calls"], word_to_index["anonymous"]])
 
-# Write file
+# Write probabilities to file
 with open("bigram_probs.txt", 'w') as file:
     file.write(str(bigram[word_to_index["the"], word_to_index["all"]]))
     file.write("\n")
@@ -52,3 +52,44 @@ with open("bigram_probs.txt", 'w') as file:
     file.write(str(bigram[word_to_index["campaign"], word_to_index["the"]]))
     file.write("\n")
     file.write(str(bigram[word_to_index["calls"], word_to_index["anonymous"]]))
+
+# Define function for generating sentences
+def GENERATE(word_index_dict, probs, model_type, max_words, start_word):
+    returnSTR = ""
+    index_word_dict = {v: k for k, v in word_index_dict.items()}
+    num_words = 0
+
+    #been passed a list of probabilities
+    if model_type == "unigram":
+
+        #using https://stackoverflow.com/questions/483666/python-reverse-invert-a-mapping
+        while(True):
+            wordIndex = np.random.choice(len(word_index_dict), 1, p=list(probs))
+            word = index_word_dict[wordIndex[0]]
+            returnSTR += word + " "
+            num_words +=1
+            if word == "</s>" or num_words == max_words:
+                break
+
+        return returnSTR
+
+    #been passed a matrix of probabilities, where each row is the previous word. 
+    if model_type == "bigram":
+        returnSTR = start_word + " "
+        prevWord = start_word
+        while(True):
+            wordIndex = np.random.choice(len(word_index_dict), 1, p=list(probs[word_index_dict[prevWord]]))
+            word = index_word_dict[wordIndex[0]]
+            returnSTR += word + " "
+            num_words +=1
+            prevWord = word
+            if word == "</s>" or num_words == max_words:
+                break
+
+        return returnSTR
+
+# Write sentences to file
+with open("bigram_generation.txt", 'w') as file:
+    for i in range(10):
+        file.write(GENERATE(word_to_index, bigram.T, "bigram", 20, "the"))
+        file.write("\n")
